e1da2f4687b06669d0108a7d50a4bb4a4db06b25
KosacekCZ <35407839+KosacekCZ@users.noreply.github.com>
2020-11-20 09:03:02 +0100

    pičovian

---
+++ Assets/Level/Scripts/LevelGeneration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    [SerializeField]private Transform levelPart_1
+
+    //vector1 = platform id, musí se nastavit v junitě
+
+    private void Awake() {
+    while() { //rule pro endpos
+    SpawnLevelPart(new Vector1(,)); //init x y
+    SpawnLevelPart(new Vector1(,) + new Vector1(,)); //init x y + rozptyl
+    SpawnLevelPart(new Vector1(,) + new Vector1(+,)); // init x y + rozptyl 2+2
+    }
+
+    private void SpawnLevelPart(Vector1, spawnPosition) {
+
+    Instantiate(levelPart_1, new Vector1(), Quaternion.identity); //constr
+    }
+
+}
